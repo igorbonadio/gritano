1363f0f6f54f8e587a232514a28c09924b2592bd
Ígor Bonadio <igorbonadio@gmail.com>
2012-11-01 23:28:47 -0200

    drying console spec

--- spec/console_spec.rb
+++ spec/console_spec.rb
@@ -1,67 +1,50 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Gritano::Console do
-  it "should respond to gritano user add igorbonadio" do
+  
+  before(:each) do
     stdin = double()
     stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:user_add)
-    console.execute("user add igorbonadio".split(' '))
+    @console = Gritano::Console.new(stdin)
+  end
+  
+  it "should respond to gritano user add igorbonadio" do
+    @console.should_receive(:user_add)
+    @console.execute("user add igorbonadio".split(' '))
   end
   
   it "should respond to gritano user rm igorbonadio" do
-    stdin = double()
-    stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:user_rm)
-    console.execute("user rm igorbonadio".split(' '))
+    @console.should_receive(:user_rm)
+    @console.execute("user rm igorbonadio".split(' '))
   end
   
   it "should respond to gritano repo add tmp/reponame.git" do
-    stdin = double()
-    stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:repo_add)
-    console.execute("repo add tmp/reponame.git".split(' '))
+    @console.should_receive(:repo_add)
+    @console.execute("repo add tmp/reponame.git".split(' '))
   end
   
   it "should respond to gritano repo rm tmp/reponame.git" do
-    stdin = double()
-    stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:repo_rm)
-    console.execute("repo rm tmp/reponame.git".split(' '))
+    @console.should_receive(:repo_rm)
+    @console.execute("repo rm tmp/reponame.git".split(' '))
   end
   
   it "should respond to gritano repo +read igorbonadio tmp/reponame.git" do
-    stdin = double()
-    stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:repo_add_read)
-    console.execute("repo +read igorbonadio tmp/reponame.git".split(' '))
+    @console.should_receive(:repo_add_read)
+    @console.execute("repo +read igorbonadio tmp/reponame.git".split(' '))
   end
   
   it "should respond to gritano repo +write igorbonadio tmp/reponame.git" do
-    stdin = double()
-    stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:repo_add_write)
-    console.execute("repo +write igorbonadio tmp/reponame.git".split(' '))
+    @console.should_receive(:repo_add_write)
+    @console.execute("repo +write igorbonadio tmp/reponame.git".split(' '))
   end
   
   it "should respond to gritano repo -read igorbonadio tmp/reponame.git" do
-    stdin = double()
-    stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:repo_remove_read)
-    console.execute("repo -read igorbonadio tmp/reponame.git".split(' '))
+    @console.should_receive(:repo_remove_read)
+    @console.execute("repo -read igorbonadio tmp/reponame.git".split(' '))
   end
   
   it "should respond to gritano repo -write igorbonadio tmp/reponame.git" do
-    stdin = double()
-    stdin.stub(:read).and_return("Your SSHKEY here...")
-    console = Gritano::Console.new(stdin)
-    console.should_receive(:repo_remove_write)
-    console.execute("repo -write igorbonadio tmp/reponame.git".split(' '))
+    @console.should_receive(:repo_remove_write)
+    @console.execute("repo -write igorbonadio tmp/reponame.git".split(' '))
   end
 end
