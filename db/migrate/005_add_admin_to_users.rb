1cbd73afaf3cb26c6006e9a14def43fb2a84d210
Ígor Bonadio <igorbonadio@gmail.com>
2012-11-24 12:45:51 -0200

    users are not admin by default

--- db/migrate/005_add_admin_to_users.rb
+++ db/migrate/005_add_admin_to_users.rb
@@ -1,6 +1,6 @@
 class AddAdminToUsers < ActiveRecord::Migration
   def up
-    add_column :users, :admin, :boolean, :default => true
+    add_column :users, :admin, :boolean, :default => false
   end
   
   def down
