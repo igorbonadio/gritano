ea6ae5d9dbce915fb8997f602797f9b59ae83773
Ígor Bonadio <igorbonadio@gmail.com>
2012-12-18 21:07:33 -0200

    Gritano::Command removed

--- features/step_definitions/command_step.rb
+++
@@ -1,9 +0,0 @@
-When /^I receive a "(.*?)" command$/ do |cmd|
-  @command = Gritano::Command.eval(cmd)
-end
-
-Then /^I should see that it is a "(.*?)": "(.*?)" "(.*?)"$/ do |access, command, repo|
-  @command[:access].to_s.should be == access
-  @command[:command].to_s.should be == command
-  @command[:repo].to_s.should be == repo if repo
-end
--- lib/gritano.rb
+++ lib/gritano.rb
@@ -2,5 +2,4 @@ ROOT_PATH = File.expand_path(File.dirname(__FILE__))
 
 require File.join(ROOT_PATH, '/gritano/models')
 require File.join(ROOT_PATH, '/gritano/console')
-require File.join(ROOT_PATH, '/gritano/command')
 require File.join(ROOT_PATH, '/gritano/cli')
