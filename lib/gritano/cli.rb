1dc92299fa3bebecef211d048e2538ea286d7c9d
Ígor Bonadio <igorbonadio@gmail.com>
2012-12-22 11:01:55 -0200

    admin:help

--- lib/gritano/cli.rb
+++ lib/gritano/cli.rb
@@ -15,6 +15,7 @@ module Gritano
     end
     
     def CLI.check(cmd, login, stdin = STDIN, home_dir = Etc.getpwuid.dir, repo_dir = Etc.getpwuid.dir)
+      Gritano::Console.bin_name('ssh git@host.com')
       console = Gritano::Console::Check.new(stdin, home_dir, repo_dir)
       begin
         output = console.execute(cmd + [login])
