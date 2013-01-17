249a2ffecf576921b1e348e532a28ff96e856283
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-17 18:51:20 -0200

    improving key spec

--- spec/model_key_spec.rb
+++ spec/model_key_spec.rb
@@ -3,6 +3,12 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 module Gritano
   describe Key do
     
+    def create_new_user_and_key
+      user = User.create(login: "igor")
+      key = user.keys.create(name: "mykey", key: "sshkey")
+      return user, key
+    end
+    
     it "should have a name" do
       key = Gritano::Key.new(key: "key")
       key.should be_invalid
@@ -14,21 +20,18 @@ module Gritano
     end
     
     it "should have a unique name per user" do
-      user = User.create(login: "igor")
-      user.keys.create(name: "mykey", key: "sshkey")
+      user, key = create_new_user_and_key
       user.keys.create(name: "mykey", key: "sshkey").should be_invalid
       user.keys.count.should == 1
     end
     
     it "should belongs to a user" do
-      user = User.create(login: "igor")
-      key = user.keys.create(name: "mykey", key: "sshkey")
+      user, key = create_new_user_and_key
       key.user.should == user
     end
     
     it "should generate the authorized_keys files" do
-      user = User.create(login: "igor")
-      key = user.keys.create(name: "mykey", key: "sshkey")
+      create_new_user_and_key
       Key.authorized_keys.should == "command=\"gritano-remote igor\" sshkey\n"
     end
   end
