f14302c6e136d4c2ffab0caaba64069a86722c7b
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-17 18:49:10 -0200

    new key spec

--- spec/model_key_spec.rb
+++ spec/model_key_spec.rb
@@ -1,18 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
-describe Gritano::Key do
-  it 'should have a name' do
-    key = Gritano::Key.new(key: "key")
-    key.should be_invalid
-  end
-
-  it 'should have a key' do
-    key = Gritano::Key.new(name: "name")
-    key.should be_invalid
-  end
-
-  it 'should generate authorized_keys file' do
-    Gritano::User.create(login: 'login').keys.create(key: "key", name: "name")
-    Gritano::Key.authorized_keys.should match /^command=/
+module Gritano
+  describe Key do
+    
+    it "should have a name" do
+      key = Gritano::Key.new(key: "key")
+      key.should be_invalid
+    end
+    
+    it "should have a ssh key" do
+      key = Gritano::Key.new(name: "name")
+      key.should be_invalid
+    end
+    
+    it "should have a unique name per user" do
+      user = User.create(login: "igor")
+      user.keys.create(name: "mykey", key: "sshkey")
+      user.keys.create(name: "mykey", key: "sshkey").should be_invalid
+      user.keys.count.should == 1
+    end
+    
+    it "should belongs to a user" do
+      user = User.create(login: "igor")
+      key = user.keys.create(name: "mykey", key: "sshkey")
+      key.user.should == user
+    end
+    
+    it "should generate the authorized_keys files" do
+      user = User.create(login: "igor")
+      key = user.keys.create(name: "mykey", key: "sshkey")
+      Key.authorized_keys.should == "command=\"gritano-remote igor\" sshkey\n"
+    end
   end
 end
