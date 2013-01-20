d566d355eb56a5bda06c6aaeb73427697957e96b
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-20 15:55:54 -0200

    refactoring: create_sqlite_config

--- lib/gritano/console/installer.rb
+++ lib/gritano/console/installer.rb
@@ -18,14 +18,18 @@ module Gritano
         Dir.mkdir(File.join(@home_dir, '.ssh')) unless File.exist?(File.join(@home_dir, '.ssh'))
       end
       
-      add_command "setup:prepare" do |argv|
-        create_gritano_dirs
+      def create_sqlite_config
         File.open(File.join(@home_dir, '.gritano', 'database.yml'), "w") do |f|
           f.write("adapter: sqlite3\ndatabase: #{File.join(Etc.getpwuid.dir, '.gritano', 'database.db')}\n")
         end
         if File.exist?(File.join(@home_dir, '.gritano', 'database.db'))
           FileUtils.rm(File.join(@home_dir, '.gritano', 'database.db'))
         end
+      end
+      
+      add_command "setup:prepare" do |argv|
+        create_gritano_dirs
+        create_sqlite_config
         return [true, "configuration has been generated"]
       end
 
