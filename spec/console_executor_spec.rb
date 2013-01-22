8da82f4eb13d172e8280e60fbfd6aa640c2e7ad7
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-21 22:15:58 -0200

    executor's specs

---
+++ spec/console_executor_spec.rb
@@ -0,0 +1,168 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+module Gritano
+  module Console
+    describe Executor do
+      def create_executor(home, repo_dir)
+        stdin = double()
+        stdin.stub(:read).and_return("Your SSHKEY here...")
+        base = Executor.new(stdin, home, repo_dir)
+      end
+      
+      it "should list users" do
+        User.should_receive(:all).and_return([])
+        create_executor('.', 'tmp').execute(["user:list"])
+      end
+      
+      it "should list users' keys" do
+        user = double()
+        user.should_receive(:keys).and_return([])
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        create_executor('.', 'tmp').execute(["user:key:list", "login"])
+      end
+      
+      it "should list users' repos" do
+        user = double()
+        user.should_receive(:repositories).and_return([])
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        create_executor('.', 'tmp').execute(["user:repo:list", "login"])
+      end
+      
+      it "should add users" do
+        user = double()
+        user.should_receive(:save).and_return(true)
+        User.should_receive(:new).with(login: "login").and_return(user)
+        create_executor('.', 'tmp').execute(["user:add", "login"])
+      end
+      
+      it "should remove users" do
+        user = double()
+        user.should_receive(:destroy).and_return(true)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        create_executor('.', 'tmp').execute(["user:rm", "login"])
+      end
+      
+      it "should add users' keys" do
+        user = double()
+        keys = double()
+        key = double()
+        key.should_receive(:valid?).and_return(true)
+        keys.should_receive(:create).with(name: "keyname", key: "Your SSHKEY here...").and_return(key)
+        user.should_receive(:keys).and_return(keys)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        create_executor('.', 'tmp').execute(["user:key:add", "login", "keyname"])
+      end
+      
+      it "should remove users' keys" do
+        keys = double()
+        users = double()
+        limit = double()
+        key = double()
+        key.should_receive(:destroy).and_return(true)
+        limit.should_receive(:limit).with(1).and_return([key])
+        users.should_receive(:where).with("users.login" => "login").and_return(limit)
+        keys.should_receive(:includes).with(:user).and_return(users)
+        Key.should_receive(:where).with(name: "keyname").and_return(keys)
+        create_executor('.', 'tmp').execute(["user:key:rm", "login", "keyname"])
+      end
+      
+      it "should add admin rights to user" do
+        user = double()
+        user.should_receive(:admin=).with(true)
+        user.should_receive(:save).and_return(true)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        create_executor('.', 'tmp').execute(["user:admin:add", "login"])
+      end
+      
+      it "should remove admin rights from users" do
+        user = double()
+        user.should_receive(:admin=).with(false)
+        user.should_receive(:save).and_return(true)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        create_executor('.', 'tmp').execute(["user:admin:rm", "login"])
+      end
+      
+      it "should list repos" do
+        Repository.should_receive(:all).and_return([])
+        create_executor('.', 'tmp').execute(["repo:list"])
+      end
+      
+      it "should create repos" do
+        repo = double()
+        repo.should_receive(:save).and_return(true)
+        Repository.should_receive(:new).with(name: "repo.git", path: "tmp").and_return(repo)
+        create_executor('.', 'tmp').execute(["repo:add", "repo.git"])
+      end
+      
+      it "should create repos and add a user" do
+        repo = double()
+        repo.should_receive(:save).and_return(true)
+        Repository.should_receive(:new).with(name: "repo.git", path: "tmp").and_return(repo)
+        
+        user = double()
+        user.should_receive(:add_access).twice
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        
+        create_executor('.', 'tmp').execute(["repo:add", "repo.git", "login"])
+      end
+      
+      it "should list repos' users" do
+        repo = double()
+        repo.should_receive(:users).and_return([])
+        Repository.should_receive(:find_by_name).with("repo.git").and_return(repo)
+        create_executor('.', 'tmp').execute(["repo:user:list", "repo.git"])
+      end
+      
+      it "should remove repos" do
+        repo = double()
+        repo.should_receive(:destroy).and_return(true)
+        Repository.should_receive(:find_by_name).with("repo.git").and_return(repo)
+        create_executor('.', 'tmp').execute(["repo:rm", "repo.git"]) 
+      end
+      
+      it "should allow user to read a repo" do
+        repo = double()
+        Repository.should_receive(:find_by_name).with("repo.git").and_return(repo)
+        
+        user = double()
+        user.should_receive(:add_access).with(repo, :read).and_return(true)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        
+        create_executor('.', 'tmp').execute(["repo:read:add", "repo.git", "login"]) 
+      end
+      
+      it "should allow user to write to a repo" do
+        repo = double()
+        Repository.should_receive(:find_by_name).with("repo.git").and_return(repo)
+        
+        user = double()
+        user.should_receive(:add_access).with(repo, :write).and_return(true)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        
+        create_executor('.', 'tmp').execute(["repo:write:add", "repo.git", "login"])
+      end
+      
+      it "should deny user to read a repo" do
+        repo = double()
+        Repository.should_receive(:find_by_name).with("repo.git").and_return(repo)
+        
+        user = double()
+        user.should_receive(:remove_access).with(repo, :read).and_return(true)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        
+        create_executor('.', 'tmp').execute(["repo:read:rm", "repo.git", "login"])
+      end
+      
+      it "should deny user to write to a repo"do
+        repo = double()
+        Repository.should_receive(:find_by_name).with("repo.git").and_return(repo)
+        
+        user = double()
+        user.should_receive(:remove_access).with(repo, :write).and_return(true)
+        User.should_receive(:find_by_login).with("login").and_return(user)
+        
+        create_executor('.', 'tmp').execute(["repo:write:rm", "repo.git", "login"])
+      end
+    end
+  end
+end
