217bc6dc18653c3f778411e18cf148734bb6f5e8
Ígor Bonadio <igorbonadio@gmail.com>
2013-01-23 15:05:31 -0200

    cli's specs

---
+++ spec/cli_spec.rb
@@ -0,0 +1,26 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+module Gritano
+  describe CLI do
+    
+    def test_console(console_class)
+      
+    end
+    
+    it "should execute local commands" do
+      console = double()
+      console.should_receive(:execute).and_return([true, "ok"])
+      Gritano::Console.should_receive(:remote_console).with(false)
+      Gritano::Console::Gritano.should_receive(:new).and_return(console)
+      CLI.execute(["user:list"])
+    end
+    
+    it "should execute remote commands" do
+      console = double()
+      console.should_receive(:execute).and_return([true, "ok"])
+      Gritano::Console.should_receive(:remote_console).with(true)
+      Gritano::Console::Remote.should_receive(:new).and_return(console)
+      CLI.check(["repo:list"], "login")
+    end
+  end
+end
