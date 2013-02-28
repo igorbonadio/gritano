6e1eae8bdf28e8823791da1a2a1f6f851df04dca
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-28 14:52:24 -0300

    ssh plugin creates a gritano-pub-key script

--- lib/gritano/plugin/ssh.rb
+++ lib/gritano/plugin/ssh.rb
@@ -7,11 +7,14 @@ module Gritano
     def on_add
       File.open(File.join(@home_dir, '.gritano', 'config.yml'), "w").write({'ssh' => true}.to_yaml)
       File.open(File.join(@ssh_path, 'authorized_keys'), "w").write('')
+      gritano_pub_key_path = `which gritano-pub-key`[0..-2]
+      `ln -s #{gritano_pub_key_path} #{File.join(@home_dir, '.gritano', 'gritano-pub-key')}`
     end
     
     def on_remove
       File.open(File.join(@home_dir, '.gritano', 'config.yml'), "w").write({'ssh' => false}.to_yaml)
       File.open(File.join(@ssh_path, 'authorized_keys'), 'w').write(Key.authorized_keys)
+      FileUtils.rm(File.join(@home_dir, '.gritano', 'gritano-pub-key')) if File.exist?(File.join(@home_dir, '.gritano', 'gritano-pub-key'))
     end
     
     add_command "install" do |params|
