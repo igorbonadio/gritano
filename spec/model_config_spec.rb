4cfc986397a5af2e245889066b3b21659f68c7c5
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-28 10:20:07 -0300

    config is not a class

--- spec/model_config_spec.rb
+++ spec/model_config_spec.rb
@@ -1,7 +1,7 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 module Gritano
-  describe Config do
+  describe "Config" do
     it "should have a ssh configuration" do
       config = YAML::load(File.open(File.join('.gritano', 'config.yml')))
       config['ssh'].should be_false
