db7a443d2e8b672d5301879db56e28821dcb397f
Ígor Bonadio <igorbonadio@gmail.com>
2013-02-28 21:37:44 -0300

    spec fix

--- spec/plugin_ssh_spec.rb
+++ spec/plugin_ssh_spec.rb
@@ -3,7 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 module Gritano
   describe Ssh do
     it "should show information" do
-      Ssh.info.should be == "Install a patched OpenSSH version used by Gritano that enables SSH lookup for public keys in a database"
+      Ssh.info.should be == "It installs a patched OpenSSH version used by Gritano that enables SSH lookup for public keys in a database"
     end
     
     it "should show the help" do
