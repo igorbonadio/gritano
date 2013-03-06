2aa0a1106f46b62baf864a08c6b9972a86f2899a
Ígor Bonadio <igorbonadio@gmail.com>
2013-03-06 16:44:00 -0300

    removing some prints

--- lib/gritano/console/gritano.rb
+++ lib/gritano/console/gritano.rb
@@ -21,18 +21,14 @@ module Gritano
       end
       
       add_command "plugin:list" do |argv|
-        begin
-          msg = Terminal::Table.new do |t|
-            t << ['plugin', 'installed']
-            t << :separator
-            Plugin.list.each do |plugin, params|
-              t.add_row [plugin, params[:installed].call]
-            end
+        msg = Terminal::Table.new do |t|
+          t << ['plugin', 'installed']
+          t << :separator
+          Plugin.list.each do |plugin, params|
+            t.add_row [plugin, params[:installed].call]
           end
-          return [true, msg]
-        rescue Exception => e
-          puts e
         end
+        return [true, msg]
       end
       
       add_command "plugin:info", "plugin_name" do |argv|
