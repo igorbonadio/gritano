31edd76be83a388334d351059dfbd413f3f89fa0
Ígor Bonadio <igorbonadio@gmail.com>
2013-04-02 11:50:04 -0300

    config spec

---
+++ spec/config_spec.rb
@@ -0,0 +1,9 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+module Gritano
+  describe Config do
+    it "should add parameters"
+    it "should remove parameters"
+    it "should modify parameters"
+  end
+end
