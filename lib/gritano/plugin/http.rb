2d40cf4abb52a760e6b55d3d5505ac3e4b4f1849
Ígor Bonadio <igorbonadio@gmail.com>
2013-05-06 15:36:51 -0300

    gritano can install the grack app

--- lib/gritano/plugin/http.rb
+++ lib/gritano/plugin/http.rb
@@ -30,6 +30,20 @@ module Gritano
           return false
       end
     end
+
+    add_command "help" do |params|
+      Http.help
+    end
+
+    add_command "install", "install_dir" do |params|
+      install_dir, = params
+      puts "[git] Cloning"
+      if system "git clone git://github.com/igorbonadio/gritano-grack.git #{install_dir}"
+        puts "Please, edit the gritano-grack/config.yml file to finish your installation."
+        return "Done"
+      end
+      return "error"
+    end
     
   end
 end
