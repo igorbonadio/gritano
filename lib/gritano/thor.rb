d939d5eeb143e943d3aa3d90ba6c541b91e4a623
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-08 22:16:03 -0300

    changing when gritano connects to the db

--- lib/gritano/thor.rb
+++ lib/gritano/thor.rb
@@ -6,7 +6,11 @@ module Gritano
       def self.define_task(name, description="", &block)
         params = block.parameters.map { |type, name| name.upcase }.join(' ')
         desc("#{name} #{params}".rstrip, description)
-        define_method(name.to_sym, &block)
+        proc = Proc.new do |*params|
+          ActiveRecord::Base.establish_connection(YAML::load(File.open(File.join(File.dirname(__FILE__), '../../spec/development.yml'))))
+          block.call(*params) 
+        end
+        define_method(name.to_sym, proc)
       end
     end
   end
