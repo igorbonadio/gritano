a7d0edc3e7dec9ae65f1c74df94b84c73cdd6e5a
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-11 16:47:00 -0300

    remote access

--- lib/gritano/console.rb
+++ lib/gritano/console.rb
@@ -11,86 +11,117 @@ module Gritano
                  repo:read:add repo:read:rm
                  repo:write:add repo:write:rm
                  repo:user:list 
+
+                 repo:list
+                 key:list key:add key:rm
               } do
         ActiveRecord::Base.establish_connection(Config.database_connection)
       end
 
-      define_task("user:list", "list all gritano users") do
-        render_table(Gritano::Core::User.order(:login), :login, :admin)
-      end
+      if Config.remote
+        define_task("repo:list", "list all repositories") do
+          use_if_not_nil Gritano::Core::User.where(login: Config.remote_user).first do |user|
+            render_table(user.repositories.order(:name), :name)
+          end
+        end
 
-      define_task("user:add", "add a gritano user") do |login|
-        create_model(Gritano::Core::User, login: login)
-      end
+        define_task("key:list", "list all your keys") do
+          use_if_not_nil Gritano::Core::User.where(login: Config.remote_user).first do |user|
+            render_table(user.keys.order(:name), :name)
+          end
+        end
 
-      define_task("user:rm", "remove a gritano user") do |login|
-        destroy_model(Gritano::Core::User, login: login)
-      end
+        define_task("key:add", "add a key") do |key_name|
+          use_if_not_nil Gritano::Core::User.where(login: Config.remote_user).first do |user|
+            create_model(user.keys, name: key_name, key: $stdin.readlines.join)
+          end
+        end
 
-      method_option :admin, :aliases => "-a", :desc => "change admin status of a user"
-      define_task("user:update", "update user information") do |login|
-        use_if_not_nil Gritano::Core::User.where(login: login).first, valid_options?(options) do |user|
-          update_model(user, options)
+        define_task("key:rm", "remove a user's key") do |key_name|
+          use_if_not_nil Gritano::Core::User.where(login: Config.remote_user).first do |user|
+            destroy_model(user.keys, name: key_name)
+          end
         end
       end
 
-      define_task("user:key:list", "list all user's keys") do |login|
-        use_if_not_nil Gritano::Core::User.where(login: login).first do |user|
-          render_table(user.keys.order(:name), :name)
+      unless Config.remote
+        define_task("user:list", "list all gritano users") do
+          render_table(Gritano::Core::User.order(:login), :login, :admin)
         end
-      end
 
-      define_task("user:key:add", "add a user's key") do |login, key_name|
-        use_if_not_nil Gritano::Core::User.where(login: login).first do |user|
-          create_model(user.keys, name: key_name, key: $stdin.readlines.join)
+        define_task("user:add", "add a gritano user") do |login|
+          create_model(Gritano::Core::User, login: login)
         end
-      end
 
-      define_task("user:key:rm", "remove a user's key") do |login, key_name|
-        use_if_not_nil Gritano::Core::User.where(login: login).first do |user|
-          destroy_model(user.keys, name: key_name)
+        define_task("user:rm", "remove a gritano user") do |login|
+          destroy_model(Gritano::Core::User, login: login)
         end
-      end
 
-      define_task("repo:list", "list all repositories") do
-        render_table(Gritano::Core::Repository.order(:name), :name)
-      end
-
-      define_task("repo:add", "add a new repository") do |name|
-        create_model(Gritano::Core::Repository, name: name, path: Config.repository_path)
-      end
-
-      define_task("repo:rm", "remove a repository") do |name|
-        destroy_model(Gritano::Core::Repository, name: name)
-      end
-
-      define_task("repo:read:add", "add read access to a repository") do |repo_name, user_login|
-        use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
-          try_change_access(user, repo, :add, :read)
+        method_option :admin, :aliases => "-a", :desc => "change admin status of a user"
+        define_task("user:update", "update user information") do |login|
+          use_if_not_nil Gritano::Core::User.where(login: login).first, valid_options?(options) do |user|
+            update_model(user, options)
+          end
         end
-      end
 
-      define_task("repo:read:rm", "remove read access to a repository") do |repo_name, user_login|
-        use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
-          try_change_access(user, repo, :remove, :read)
+        define_task("user:key:list", "list all user's keys") do |login|
+          use_if_not_nil Gritano::Core::User.where(login: login).first do |user|
+            render_table(user.keys.order(:name), :name)
+          end
         end
-      end
 
-      define_task("repo:write:add", "add write access to a repository") do |repo_name, user_login|
-        use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
-          try_change_access(user, repo, :add, :write)
+        define_task("user:key:add", "add a user's key") do |login, key_name|
+          use_if_not_nil Gritano::Core::User.where(login: login).first do |user|
+            create_model(user.keys, name: key_name, key: $stdin.readlines.join)
+          end
         end
-      end
 
-      define_task("repo:write:rm", "remove write access to a repository") do |repo_name, user_login|
-        use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
-          try_change_access(user, repo, :remove, :write)
+        define_task("user:key:rm", "remove a user's key") do |login, key_name|
+          use_if_not_nil Gritano::Core::User.where(login: login).first do |user|
+            destroy_model(user.keys, name: key_name)
+          end
         end
-      end
 
-      define_task("repo:user:list", "list all user that have access to a repository") do |repo_name|
-        use_if_not_nil Gritano::Core::Repository.where(name: repo_name).first do |repo|
-          render_table(repo.users.order(:login), :login, :access => repo)
+        define_task("repo:list", "list all repositories") do
+          render_table(Gritano::Core::Repository.order(:name), :name)
+        end
+
+        define_task("repo:add", "add a new repository") do |name|
+          create_model(Gritano::Core::Repository, name: name, path: Config.repository_path)
+        end
+
+        define_task("repo:rm", "remove a repository") do |name|
+          destroy_model(Gritano::Core::Repository, name: name)
+        end
+
+        define_task("repo:read:add", "add read access to a repository") do |repo_name, user_login|
+          use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
+            try_change_access(user, repo, :add, :read)
+          end
+        end
+
+        define_task("repo:read:rm", "remove read access to a repository") do |repo_name, user_login|
+          use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
+            try_change_access(user, repo, :remove, :read)
+          end
+        end
+
+        define_task("repo:write:add", "add write access to a repository") do |repo_name, user_login|
+          use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
+            try_change_access(user, repo, :add, :write)
+          end
+        end
+
+        define_task("repo:write:rm", "remove write access to a repository") do |repo_name, user_login|
+          use_if_not_nil Gritano::Core::User.where(login: user_login).first, Gritano::Core::Repository.where(name: repo_name).first do |user, repo|
+            try_change_access(user, repo, :remove, :write)
+          end
+        end
+
+        define_task("repo:user:list", "list all user that have access to a repository") do |repo_name|
+          use_if_not_nil Gritano::Core::Repository.where(name: repo_name).first do |repo|
+            render_table(repo.users.order(:login), :login, :access => repo)
+          end
         end
       end
 
