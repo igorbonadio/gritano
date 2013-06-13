da2d3a0e16406fc471e13271675cd2f897ff2564
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-13 10:17:15 -0300

    more specs for git console

--- spec/gritano/config_spec.rb
+++ spec/gritano/config_spec.rb
@@ -9,9 +9,25 @@ module Gritano::CLI
   end
 
   describe "Config" do
-    it "should start a remote terminal app" do
-      Gritano::CLI::Console::Remote.should_receive(:start)
-      Gritano::CLI.configure { |config| config.remote = true }
+
+    describe "#remote" do
+      it "should start a remote terminal app if the command is a gritano command" do
+        Gritano::CLI::Console::Remote.should_receive(:start)
+        ARGV[0] = "some:command"
+        Gritano::CLI.configure { |config| config.remote = true }
+      end
+
+      it "should start a git terminal app if the command is git:receive:pack" do
+        Gritano::CLI::Console::Git.should_receive(:start)
+        ARGV[0] = "git:receive:pack"
+        Gritano::CLI.configure { |config| config.remote = true }
+      end
+
+      it "should start a git terminal app if the command is git:upload:pack" do
+        Gritano::CLI::Console::Git.should_receive(:start)
+        ARGV[0] = "git:upload:pack"
+        Gritano::CLI.configure { |config| config.remote = true }
+      end
     end
 
     it "should start a local terminal app" do
