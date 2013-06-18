b46fb4b5d64f90bc2f140b9574df5cdcd570a40d
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-17 23:19:47 -0300

    config specs

---
+++ spec/gritano/config_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+module Gritano::CLI
+  describe Config do
+    it "should have default parameters" do
+      Config.should_receive(:database_connection=)
+      Config.should_receive(:repository_path=)
+      Config.should_receive(:remote_ssh_prefix=)
+      Gritano::CLI.configure {}
+    end
+  end
+end
