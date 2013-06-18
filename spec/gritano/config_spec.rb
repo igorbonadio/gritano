172778ec2b241120d40f154efb7a74a47b6cc9c8
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-17 23:34:07 -0300

    fixing file#open in config_spec.rb

--- spec/gritano/config_spec.rb
+++ spec/gritano/config_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 module Gritano::CLI
   describe Config do
     it "should have default parameters" do
+      File.stub(:open)
       Config.should_receive(:database_connection=)
       Config.should_receive(:repository_path=)
       Config.should_receive(:remote_ssh_prefix=)
